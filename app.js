7c25831913bc9eb6476bdcebc5134737ef72f219
usmanjamil83 <usmanjamil83@hotmail.com>
2017-04-14 18:05:16 -0400

    worked on app.js

--- app.js
+++ app.js
@@ -1,26 +1,56 @@
 var fs = require('fs');
 var inquirer = require('inquirer');
 
-flashCards()
+flashCards();
 
+function flashCards(){
+	if (process.argv[2] === 'basic'){
+		inquirer.prompt([{
+			name: "front",
+			message: "Enter the question: "
+		},{
+			name: "back",
+			message: "Enter the answer: " 
+		}]).then(function(answer){
+			var BasicCard = new BasicCard(answer.front, answer.back);
+			BasicCard();
+		});
+	}
+	else if (process.argv[2] === "cloze"){
+		inquirer.prompt([{
+			name: "text",
+			message: "Enter full text:"
+		},{
+			name: "cloze",
+			message: "Enter cloze text:"
+		}]).then(function(answer){
+			var ClozeCard = new ClozeCard(answer.text, answer.cloze);
+			ClozeCard();
+		});
+	}
+	else if (process.argv[2] === "show"){
+		showthecards();
+	}
+	else {
+		console.log("Type one of these options 'basic' , 'cloze' , 'show' ");
+	}
+}
 function BasicCard(front, back){
 	this.front = front;
-	this.back = bcak;
-	this.card = function(){
+	this.back = back;
+	this.save = function(){
 		fs.appendFile("card.txt", "front: " + front + " back: " + back , function(err) {
 			if (err) {
 				return console.log(err);
 			}
 			console.log("card.txt was updated!");
-
 		});
 	};
 }
-
 function ClozeCard(text, cloze){
 	this.text = text;
 	this.cloze = cloze;
-	this.card = function(){
+	this.save = function(){
 		fs.appendFile("card.txt", "text: " + text + " cloze: " + cloze , function(err) {
 			if (err) {
 				return console.log(err);
@@ -37,30 +67,13 @@ ClozeCard.prototype.partial = function(){
 		return 'wrong input';
 	}
 };
-
-function flashCards(){
-	if (process.argv[2] === 'basic'){
-		inquirer.prompt([{
-			name: "Front:",
-			message: "Enter the question: "
-		},{
-			name: "Back:",
-			message: "Enter the answer: " 
-		}]).then(function(answer){
-			var BasicCard = new BasicCard(answer.front, answer.back);
-			BasicCard.card();
-		});
-	}
-	else if (process.argv[2] === "cloze"){
-		inquirer.prompt([{
-			name: "Text:",
-			message: "Enter full text:"
-		},{
-			name: "Cloze",
-			message: "Enter cloze text:"
-		}]).then(function(answer){
-			var ClozeCard = new ClozeCard(answer.text, answer.cloze);
-			ClozeCard.card();
-		});
-	}
+function showthecards() {
+	fs.readFile("card.txt", "utf8", function(err, data){
+		if (err){
+			console.log(err);
+		}
+		else {
+			console.log(data);
+		}
+	});
 }
