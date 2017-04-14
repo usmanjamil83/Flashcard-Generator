9398307bd6b092a007b5cb0eb2ca5c0516f46d87
usmanjamil83 <usmanjamil83@hotmail.com>
2017-04-14 18:21:28 -0400

    added comments in app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+// npm packages.
 var fs = require('fs');
 var inquirer = require('inquirer');
 
+// calling the function flashCards.
 flashCards();
 
+// creating a function flashCards.
 function flashCards(){
+	// using if statement to check the user responce.
 	if (process.argv[2] === 'basic'){
+		// using prompt to get the user input for the questions.
 		inquirer.prompt([{
 			name: "front",
 			message: "Enter the question: "
@@ -12,11 +17,14 @@ function flashCards(){
 			name: "back",
 			message: "Enter the answer: " 
 		}]).then(function(answer){
+			// creating a new object of BasicCard
 			var BasicCard = new BasicCard(answer.front, answer.back);
 			BasicCard();
 		});
 	}
+	// using else if statement to check the user responce
 	else if (process.argv[2] === "cloze"){
+		// using prompt to get the user input for the questions.
 		inquirer.prompt([{
 			name: "text",
 			message: "Enter full text:"
@@ -24,10 +32,12 @@ function flashCards(){
 			name: "cloze",
 			message: "Enter cloze text:"
 		}]).then(function(answer){
+			// creating a new object of ClozeCard
 			var ClozeCard = new ClozeCard(answer.text, answer.cloze);
 			ClozeCard();
 		});
 	}
+	// using else if statement to check the user responce
 	else if (process.argv[2] === "show"){
 		showthecards();
 	}
@@ -35,11 +45,14 @@ function flashCards(){
 		console.log("Type one of these options 'basic' , 'cloze' , 'show' ");
 	}
 }
+// creating a BasicCard constructor
 function BasicCard(front, back){
 	this.front = front;
 	this.back = back;
 	this.save = function(){
+		// appending the user input into card.txt
 		fs.appendFile("card.txt", "front: " + front + " back: " + back , function(err) {
+			// catching an error
 			if (err) {
 				return console.log(err);
 			}
@@ -47,11 +60,14 @@ function BasicCard(front, back){
 		});
 	};
 }
+// creating a ClozeCard constructor
 function ClozeCard(text, cloze){
 	this.text = text;
 	this.cloze = cloze;
 	this.save = function(){
+		// appending the user input into card.txt
 		fs.appendFile("card.txt", "text: " + text + " cloze: " + cloze , function(err) {
+			// catching an error
 			if (err) {
 				return console.log(err);
 			}
@@ -59,6 +75,7 @@ function ClozeCard(text, cloze){
 		});
 	};
 }
+// using prototype to give new property to ClozeCard
 ClozeCard.prototype.partial = function(){
 	if(this.text.includes(this.cloze)){
 		return this.text.replace(this.cloze, "....");
@@ -67,6 +84,7 @@ ClozeCard.prototype.partial = function(){
 		return 'wrong input';
 	}
 };
+// showing all the saved card in card.txt using readFile
 function showthecards() {
 	fs.readFile("card.txt", "utf8", function(err, data){
 		if (err){
